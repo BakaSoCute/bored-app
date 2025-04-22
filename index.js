3370ec5b5b1411a05ec959fc6bc0c4d665573c8b
Aleksandr Omelucik <bladewizard@mail.ru>
2025-04-22 08:19:24 +0300

    final commit

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const titleNode = document.getElementById("title");
 const h2TitleNode = document.getElementById("changeTitle");
 const buttonNode = document.getElementById("button");
+const mainNode = document.getElementById("main");
 
 
 const borred = () => {
     titleNode.innerText ="Ура, теперь не скучно!";
-    fetch("https://www.boredapi.com/documentation")
+    fetch("https://api.adviceslip.com/advice")
     .then ((response) =>{
         return response.json();
     })
-    .then
+    .then ((data) => {
+        console.log(data.slip.advice)
+        h2TitleNode.innerText = `${data.slip.advice}`
+    })
+    mainNode.classList = "main2"
 }
 
-buttonNode.addEventListener("click" ,borred());
+buttonNode.addEventListener("click" ,borred);
